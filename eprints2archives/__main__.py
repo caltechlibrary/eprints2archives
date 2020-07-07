9c45eb8d566b7e6404cfd6794b36a70edfc71d3f
Michael Hucka <mhucka@caltech.edu>
2020-07-07 15:57:51 -0700

    Start a main

---
+++ eprints2archives/__main__.py
@@ -0,0 +1,15 @@
+'''
+__main__: main command-line interface to eprints2archives
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2020 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
