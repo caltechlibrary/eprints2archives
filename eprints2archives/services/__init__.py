f302fda69575cfa24f14d0ab5d64dd4f35b9362d
Michael Hucka <mhucka@caltech.edu>
2020-07-07 16:21:49 -0700

    First pass at __init__.py for services

---
+++ eprints2archives/services/__init__.py
@@ -0,0 +1,31 @@
+'''
+eprints2archives interfaces to web archiving services.
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2020 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+from .internetarchive import SendToInternetArchive
+from .archivetoday import SendToArchiveToday
+from .archivest import SentToArchiveST
+
+KNOWN_SERVICES = {
+    'internetarchive' : SendToInternetArchive,
+    'archive.st'      : SendToArchiveST,
+    'archive.today'   : SendToArchiveToday,
+}
+
+# Save this list to avoid recreating it all the time.
+SERVICES_LIST = sorted(KNOWN_SERVICES.keys())
+
+def services_list():
+    return SERVICES_LIST
